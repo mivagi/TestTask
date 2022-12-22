09063dde4f7e5a9a23bc432f3f5e43561eff7222
praction839 <praction839@gmail.com>
2022-12-22 13:09:32 +0300

    Добавьте файлы проекта.

---
+++ TestTask/Class1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace TestTask
+{
+    public class Figure
+    {
+        private const double PI = 3.14f;
+        private double area;
+        //Метод для вычисления площади круга.
+        public void FigureArea(double r)
+        {
+            area = PI * Math.Pow(r, 2);
+            Console.WriteLine($"Площадь круга равна {area}");
+        }
+        //Метод для вычисления площади треугольника.
+        public void FigureArea(double a, double b, double c)
+        {
+            var p = (a + b + c) / 2;
+            area = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+            Console.WriteLine($"Площадь треугольника равна {area}");
+            var max = a;
+            if (b > a)
+                max = b;
+            if (c > max)
+                max = c;
+            if (max == a)
+            {
+                if (Math.Pow(a, 2) == Math.Pow(b, 2) + Math.Pow(c, 2))
+                    Console.WriteLine("Треугольник прямоугольный");
+                else
+                    Console.WriteLine("Треугольник не прямоугольный");
+            }
+            else if (max == b)
+            {
+                if (Math.Sqrt(b) == Math.Sqrt(a) + Math.Sqrt(c))
+                    Console.WriteLine("Треугольник прямоугольный");
+                else
+                    Console.WriteLine("Треугольник не прямоугольный");
+            }
+            else if (max == c)
+            {
+                if (Math.Pow(c, 2) == Math.Pow(b, 2) + Math.Pow(a, 2))
+                    Console.WriteLine("Треугольник прямоугольный");
+                else
+                    Console.WriteLine("Треугольник не прямоугольный");
+            }
+        }
+    }
+}
+/*Вопрос №3:
+ * Есть таблицы Category(с полями id и name) и Product(с полями id и name)
+ * Для связи многие ко многим надо создать дополнительную 
+ * таблицу CategoriesAndProducts с полями id, categoryId и productId
+ * (поле id необходимо для того что бы поля categoryId и productId можно было делать равными null
+ * И тогда запрос будет выглядеть так:
+ * SELECT Product.name, Category.name FROM Product
+ * JOIN CategoriesAndProducts
+ * ON Product.id = CategoriesAndProducts.productId
+ * LEFT JOIN Category
+ * ON Category.id = CategoriesAndProducts.categoryId;
+ */
